ec0a78a667b23317bcabb204ba19624cc8821f0f
JaiVenkateshwaraTechnologies <kuldeephadnoorkat@gmail.com>
2020-11-10 16:49:15 +0530

    ADD UC2 user Input for the Symbol

--- tictactoe.java
+++ tictactoe.java
@@ -1,14 +1,44 @@
 public class tictactoe {
 	public static char[] Board() {
 		char[] gameLayout = new char[10];
-		for(int i=0 ; i<gameLayout.length ;i++)
-			gameLayout[i]='-';
+		for(int i=1 ; i<gameLayout.length ; i++){
+			gameLayout[i]=' ';
+		}
 		return gameLayout;
+	}
+
+	public static void symbolAssignment() {
+		char Player;
+		char Computer;
+		Scanner sc = new Scanner(System.in);
+
+		while(True){
+			System.out.println(" Choose X or O ");
+			char symbol = sc.next().Uppercase().charAt(0);
+
+
+				if(symbol == 'X'){
+
+					Player='X';
+					Computer='O';
+
+				}
+				else if(symbol == 'O'){
+					Computer='X';
+					Player='O';
+
+				}
+				else{
+					System.out.println("Game Doesnt Support Inputs Expect X and Y");
+				}
+		}
+
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		Board();
+		symbolAssignment();
 	}
 
 }
